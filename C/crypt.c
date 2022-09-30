d97af0c55c93025f1d053453b56ebe747b950322
Index <nsxz1359@naver.com>
2022-09-30 17:23:56 +0900

    Update crypt.c

--- C/crypt.c
+++ C/crypt.c
@@ -110,7 +110,7 @@ void help()
     printf("    %-16s   %30s\n","-i <file/directory>","Input File or Directory to encrypt");
     printf("    %-16s   %30s\n","-o <file/directory>","Output File or Directory for save the result");
     printf("    %-16s   %37s\n","-a <algorithm>","Encrypt file by selected algorithm");
-    printf("                          Support openssl-dgst, AES-[128|192|256]-[CBC|ECB]");
+    printf("                          Support openssl-dgst(no md4), AES-[128|192|256]-[CBC|ECB]");
     printf("\n    %-16s %32s\n","-d","Decrypt (only AES possible)");
     printf("    %-16s %12s\n","-v","Verbose");
 }
