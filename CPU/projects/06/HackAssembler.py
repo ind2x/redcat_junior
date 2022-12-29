2f03ca0522b57d91b4a742947164d3dbb63a3921
Index <nsxz1359@naver.com>
2022-12-29 21:12:28 +0900

    Update HackAssembler.py

--- CPU/projects/06/HackAssembler.py
+++ CPU/projects/06/HackAssembler.py
@@ -1,5 +1,5 @@
 import sys      # sys.argv
-import string   # isnumeric, islower, index
+import string   # isnumeric
 
 '''
 Need to handle: 
@@ -10,15 +10,21 @@ Need to handle:
 
 # Computation Specification       
 def comp(comp):
-    comp_bit = ['101010', '111111', '111010', '001100', '110000', '001101', '110001', '001111', 
-                '110011', '011111', '110111', '001110', '110010', '000010', '010011', 
-                '000111', '000000', '010101']
+    comp_bit = [
+        '101010', '111111', '111010', '001100', '110000', '001101', 
+        '110001', '001111', '110011', '011111', '110111', '001110', 
+        '110010', '000010', '010011', '000111', '000000', '010101'
+    ]
     
-    a0 = ['0', '1', '-1', 'D', 'A', '!D', '!A', '-D', '-A', 'D+1', 
-          'A+1', 'D-1', 'A-1', 'D+A', 'D-A', 'A-D', 'D&A', 'D|A']
+    a0 = [
+        '0', '1', '-1', 'D', 'A', '!D', '!A', '-D', '-A', 'D+1', 
+        'A+1', 'D-1', 'A-1', 'D+A', 'D-A', 'A-D', 'D&A', 'D|A'
+    ]
     
-    a1 = {'M':4, '!M':6, '-M':8, 'M+1':10, 'M-1':12, 'D+M':13, 
-          'D-M':14, 'M-D':15, 'D&M':16, 'D|M':17}
+    a1 = {
+        'M':4, '!M':6, '-M':8, 'M+1':10, 'M-1':12, 
+        'D+M':13, 'D-M':14, 'M-D':15, 'D&M':16, 'D|M':17
+    }
 
     if comp in a0:
         return '0' + comp_bit[a0.index(comp)]
@@ -36,10 +42,12 @@ def jump(jump):
     return '{0:03b}'.format(jump_field.index(jump)+1)
 
 # predefined symbols
-symbol_table = {'R0':0, 'R1':1, 'R2':2, 'R3':3, 'R4':4, 'R5':5, 'R6':6, 'R7':7, 'R8':8, 
-                'R9':9, 'R10':10, 'R11':11, 'R12':12, 'R13':13, 'R14':14, 'R15':15, 
-                'SCREEN':16384, 'KBD':24576, 
-                'SP':0, 'LCL':1, 'ARG':2, 'THIS':3, 'THAT':4}
+symbol_table = {
+    'R0':0, 'R1':1, 'R2':2, 'R3':3, 'R4':4, 'R5':5, 'R6':6, 'R7':7, 'R8':8, 
+    'R9':9, 'R10':10, 'R11':11, 'R12':12, 'R13':13, 'R14':14, 'R15':15, 
+    'SCREEN':16384, 'KBD':24576, 
+    'SP':0, 'LCL':1, 'ARG':2, 'THIS':3, 'THAT':4
+}
 
 # label이 있는 경우 label의 instruction 위치 값을 저장
 label = 0
@@ -56,7 +64,7 @@ with open(sys.argv[1],'r') as f:
     for line in lines:
         line = line.strip().split(' ')[0]
         # if White Space -> ignore
-        if line[0:2] == "//" or line == '\n' or line == '':   
+        if line[0:2] == "//" or line == '':   
             continue
         
         # if variable, add on symbol table
@@ -72,7 +80,6 @@ with open(sys.argv[1],'r') as f:
         
         n += 1
         instructions.append(line)
-        #print(line)
     
     with open("./"+ sys.argv[1].replace('asm', 'hack'), 'w') as res:
         # translate instructions to 16-bit binary code
