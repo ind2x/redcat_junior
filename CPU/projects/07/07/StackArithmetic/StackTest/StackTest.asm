617ffb32c6e6ece586405360e75c9b11244a4164
Index <nsxz1359@naver.com>
2022-12-30 15:19:46 +0900

    Add files via upload

---
+++ CPU/projects/07/07/StackArithmetic/StackTest/StackTest.asm
@@ -0,0 +1,452 @@
+// push constant 17
+@17
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 17
+@17
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// eq
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=D-M
+@TRUE0
+D;JEQ
+D=0
+@END0
+0;JMP
+(TRUE0)
+    D=-1
+(END0)
+    @SP
+    A=M
+    M=D
+    @SP
+    M=M+1
+// push constant 17
+@17
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 16
+@16
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// eq
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=D-M
+@TRUE1
+D;JEQ
+D=0
+@END1
+0;JMP
+(TRUE1)
+    D=-1
+(END1)
+    @SP
+    A=M
+    M=D
+    @SP
+    M=M+1
+// push constant 16
+@16
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 17
+@17
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// eq
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=D-M
+@TRUE2
+D;JEQ
+D=0
+@END2
+0;JMP
+(TRUE2)
+    D=-1
+(END2)
+    @SP
+    A=M
+    M=D
+    @SP
+    M=M+1
+// push constant 892
+@892
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 891
+@891
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// lt
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=D-M
+@TRUE3
+D;JGT
+D=0
+@END3
+0;JMP
+(TRUE3)
+    D=-1
+(END3)
+    @SP
+    A=M
+    M=D
+    @SP
+    M=M+1
+// push constant 891
+@891
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 892
+@892
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// lt
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=D-M
+@TRUE4
+D;JGT
+D=0
+@END4
+0;JMP
+(TRUE4)
+    D=-1
+(END4)
+    @SP
+    A=M
+    M=D
+    @SP
+    M=M+1
+// push constant 891
+@891
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 891
+@891
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// lt
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=D-M
+@TRUE5
+D;JGT
+D=0
+@END5
+0;JMP
+(TRUE5)
+    D=-1
+(END5)
+    @SP
+    A=M
+    M=D
+    @SP
+    M=M+1
+// push constant 32767
+@32767
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 32766
+@32766
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// gt
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=D-M
+@TRUE6
+D;JLT
+D=0
+@END6
+0;JMP
+(TRUE6)
+    D=-1
+(END6)
+    @SP
+    A=M
+    M=D
+    @SP
+    M=M+1
+// push constant 32766
+@32766
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 32767
+@32767
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// gt
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=D-M
+@TRUE7
+D;JLT
+D=0
+@END7
+0;JMP
+(TRUE7)
+    D=-1
+(END7)
+    @SP
+    A=M
+    M=D
+    @SP
+    M=M+1
+// push constant 32766
+@32766
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 32766
+@32766
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// gt
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=D-M
+@TRUE8
+D;JLT
+D=0
+@END8
+0;JMP
+(TRUE8)
+    D=-1
+(END8)
+    @SP
+    A=M
+    M=D
+    @SP
+    M=M+1
+// push constant 57
+@57
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 31
+@31
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 53
+@53
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// add
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=D+M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 112
+@112
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// sub
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=M-D
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// neg
+@SP
+M=M-1
+A=M
+M=-M
+@SP
+M=M+1
+// and
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=D&M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 82
+@82
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// or
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=D|M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// not
+@SP
+M=M-1
+A=M
+M=!M
+@SP
+M=M+1
