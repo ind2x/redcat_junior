069992b21fbd7fbb5d610993183bf46afbb6d0b0
Index <nsxz1359@naver.com>
2023-03-13 13:25:44 +0900

    Update JackAnalyzer.py

--- CPU/projects/10/JackAnalyzer.py
+++ CPU/projects/10/JackAnalyzer.py
@@ -12,11 +12,17 @@ keyword = ['class', 'constructor', 'function', 'method', 'field', 'static', 'var
 symbol = ['{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', 
           '|', '<', '>', '=', '~'] 
 
+# check which close tag must i use before or after(class) parse '}'
+classOn = subroutineBodyOn = 0
+IfElseWhile = []
+
 def replace(parsed_code, code):
     return parsed_code.replace('$$', code, 1)
 
 # <, >, ", and & are outputted as &lt;, &gt;, &quot;, and &amp;
 def tokenizer(parsed_code, code):
+    global classOn, subroutineBodyOn, IfElseWhile
+
     if code in keyword:
         return replace(parsed_code, "<keyword> {} </keyword>\n$$".format(code))
     elif code in symbol:
@@ -26,9 +32,31 @@ def tokenizer(parsed_code, code):
             return replace(parsed_code, "<symbol> {} </symbol>\n$$".format("&gt;"))
         # if symbol is ';' or '}', then line or code ends
         elif code == '}':
-            return replace(parsed_code, "<symbol> {} </symbol>\n".format(code))
-        else:
-            return replace(parsed_code, "<symbol> {} </symbol>\n$$".format(code))
+            if len(IfElseWhile) != 0:
+                statement = IfElseWhile.pop()
+                parsed_code = replace(parsed_code, "</statements>\n$$")
+                parsed_code = replace(parsed_code, "<symbol> } </symbol>\n$$")
+
+                if statement == 'while':
+                    parsed_code = replace(parsed_code, "</whileStatement>\n$$")
+                else: # if, else
+                    parsed_code = replace(parsed_code, "</ifStatement>\n$$")
+
+            elif subroutineBodyOn:
+                subroutineBodyOn = 0
+                # add </statements>\n<symbol> } </symbol>\n
+                # </subroutineBody>\n</subroutineDec>\n
+                parsed_code = replace(parsed_code, "</statements>\n$$")
+                parsed_code = replace(parsed_code, "<symbol> } </symbol>\n$$")
+                parsed_code = replace(parsed_code, "</subroutineBody>\n$$")
+                parsed_code = replace(parsed_code, "</subroutineDec>\n$$")
+            
+            elif classOn:
+                classOn = 0
+                parsed_code = replace(parsed_code, "<symbol> } </symbol>\n$$")
+                parsed_code = replace(parsed_code, "</class>")
+
+            return parsed_code
     else:
         if code.isnumeric():
             return replace(parsed_code, "<integerConstant> {} </integerConstant>\n$$".format(code))
@@ -116,7 +144,6 @@ def expression(parsed_code, code):
     # subroutineCall - (className|varName)'.'subroutineName '(' expressionList ')'
     elif re.search('(.+)(\.)(.+)\((.?|.+)\)', code):
         # add <term>
-        #print(code)
         parsed_code = replace(parsed_code, "<term>\n$$")
         parsed_code = subroutineCall(parsed_code, code, 2)
         parsed_code = replace(parsed_code, "</term>\n$$")
@@ -146,15 +173,22 @@ def expression(parsed_code, code):
 
     # if op exists
     elif code in ['+', '-', '*', '/', '&', '|', '<', '>', '=']:
-        # split to "term (op term)"
-        expr = re.search('(.+?)\s(.+?)\s(\(.+?\))', code)
-        
-        # expression term1
-        parsed_code = expression(parsed_code, expr.group(1))
-        # parse op
-        parsed_code = tokenizer(parsed_code, expr.group(2))
-        # expression term2
-        parsed_code = expression(parsed_code, expr.group(3))
+        # if unaryOp term
+        if code[0] == '-' or code[0] == '~':
+            code = re.search('(-|~)(.+)', code)
+            parsed_code = tokenizer(parsed_code, code.group(1))
+            parsed_code = replace(parsed_code, "<term>\n$$")
+            parsed_code = tokenizer(parsed_code, code.group(2))
+            parsed_code = replace(parsed_code, "</term>\n$$")
+        else: 
+            # split to "term (op term)"
+            expr = re.search('(.+?)\s(.+?)\s(.+?)', code)
+            # expression term1
+            parsed_code = expression(parsed_code, expr.group(1))
+            # parse op
+            parsed_code = tokenizer(parsed_code, expr.group(2))
+            # expression term2
+            parsed_code = expression(parsed_code, expr.group(3))
 
     # varName
     else:
@@ -164,8 +198,36 @@ def expression(parsed_code, code):
 
     return parsed_code
 
+# compile classVarDec, varDec
+def classVarDecOrvarDec(parsed_code, code):
+    # split to keyword, type varName*, ';'
+    code = re.search('(.+?)\s(.+?)\s(.+)(;)', code)
+
+    parsed_code = tokenizer(parsed_code, code.group(1))
+    parsed_code = tokenizer(parsed_code, code.group(2))
+    
+    # if there is multiple varNames
+    if ',' in code.group(3):
+        par = code.group(3).split(', ')
+        
+        for idx, token in enumerate(par):
+            parsed_code = tokenizer(parsed_code, token)
+            if idx == len(par)-1:
+                continue
+            parsed_code = tokenizer(parsed_code, ',')
+    else:
+        parsed_code = tokenizer(parsed_code, code.group(3))
+    
+    # parse ';'
+    parsed_code = tokenizer(parsed_code, code.group(4))
+
+    return parsed_code
+
 def analyze(jackFile):
     global keyword, symbol
+    global classOn, subroutineBodyOn
+    
+    subroutineDecOn = 0 # use when compile subroutineBody
     parsed_code = ''    # save parsed codes
 
     with open(jackFile, 'r') as f:
@@ -178,16 +240,26 @@ def analyze(jackFile):
             
             # compile class structure
             if code[0].find('class') == 0:
+                # split to keyword, className, '{'
                 code = ''.join(code).split(' ')
                 parsed_code += "<class>\n"
                 parsed_code += "$$\n"
-                parsed_code += "</class>"
+
+                # add </class> after parse '}'
+                classOn = 1
+
                 for token in code:
                     parsed_code = tokenizer(parsed_code, token)
 
+            # compile classVarDec
+            elif code[0].find('static') == 0 or code[0].find('field') == 0:
+                parsed_code = replace(parsed_code, "<classVarDec>\n$$")
+                parsed_code = classVarDecOrvarDec(parsed_code, code[0])
+                parsed_code = replace(parsed_code, "</classVarDec>\n$$")
+
             # compile subroutineDec structure
             elif code[0].find('constructor') == 0 or code[0].find('function') == 0 or code[0].find('method') == 0:
-                parsed_code = replace(parsed_code, "<subroutineDec>\n$$</subroutineDec>")
+                parsed_code = replace(parsed_code, "<subroutineDec>\n$$")
 
                 # split to keyword, type, subroutineName, parameter, '{'
                 code = re.search('(.+)\s(.+)\s(.+)\((.?|.+)\)\s({)', code[0])
@@ -225,49 +297,26 @@ def analyze(jackFile):
                 parsed_code = replace(parsed_code, "</parameterList>\n$$")
                 parsed_code = tokenizer(parsed_code, ')')
 
+                subroutineDecOn = 1
                 # add <subroutineBody>
-                parsed_code = replace(parsed_code, "<subroutineBody>\n$$</subroutineBody>\n$$")
-                
-                parsed_code = replace(parsed_code, "<statements>\n$$")
-                
+                parsed_code = replace(parsed_code, "<subroutineBody>\n$$")
                 # '{'
-                parsed_code = tokenizer(parsed_code, code.group(5))
+                parsed_code = tokenizer(parsed_code, '{')
+                subroutineBodyOn = 1
 
-            # classVarDec and varDec
-            elif code[0].find('static') == 0 or code[0].find('field') == 0 or code[0].find('var') == 0:
-                varOn = 0   # check if static, field or var
-                if code[0].find('static') == 0 or code[0].find('field') == 0:
-                    parsed_code = replace(parsed_code, "<classVarDec>\n$$")
-                else:
-                    parsed_code = replace(parsed_code, "<varDec>\n$$")
-                    varOn = 1
-                
-                # extract keyword, type
-                code = re.search('(.+?)\s(.+?)\s(.+)(;)', code[0])
-                parsed_code = tokenizer(parsed_code, code.group(1))
-                parsed_code = tokenizer(parsed_code, code.group(2))
+            # compile varDec
+            elif code[0].find('var') == 0:
+                parsed_code = replace(parsed_code, "<varDec>\n$$")
+                parsed_code = classVarDecOrvarDec(parsed_code, code[0])
+                parsed_code = replace(parsed_code, "</varDec>\n$$")
 
-                # if there is multiple varNames
-                if ',' in code.group(3):
-                    par = code.group(3).split(', ')
-                    for idx, token in enumerate(par):
-                        parsed_code = tokenizer(parsed_code, token)
-                        if idx == len(par)-1:
-                            continue
-                        parsed_code = tokenizer(parsed_code, ',')
-                else:
-                    parsed_code = tokenizer(parsed_code, code.group(3))
+            # if no varDec, add <statements>
+            elif subroutineDecOn == 1:
+                parsed_code = replace(parsed_code, "<statements>\n$$")
+                subroutineDecOn = 0
 
-                # parse ';'
-                parsed_code = tokenizer(parsed_code, code.group(4))
-
-                if varOn == 1:
-                    parsed_code = replace(parsed_code, "</varDec>\n$$")
-                else:
-                    parsed_code = replace(parsed_code, "</classVarDec>\n$$")
-            
             # compile statements
-            if code[0].find('let') == 0 or code[0].find('if') == 0 or code[0].find('while') == 0 or code[0].find('do') == 0 or code[0].find('return') == 0 :
+            elif code[0].find('let') == 0 or code[0].find('if') == 0 or code[0].find('while') == 0 or code[0].find('do') == 0 or code[0].find('return') == 0 :
                 # compile let statement
                 if code[0].find('let') == 0:
                     parsed_code = replace(parsed_code, "<letStatement>\n$$")
@@ -293,41 +342,100 @@ def analyze(jackFile):
                     # parse ';'
                     parsed_code = tokenizer(parsed_code, code.group(5))
                     parsed_code = replace(parsed_code, "</letStatement>\n$$")
-                '''
-                # if statement 
+
+                # compile if statement 
+                # 'if' '(' expression ')' '{' statements '}' 'else' '{' statements '}'
+                # code[0] == if () {
                 elif code[0].find('if') == 0:
+                    IfElseWhile.append('if')
+
                     # add <ifstatement>
                     parsed_code = replace(parsed_code, "<ifStatement>\n$$")
-
-                    expr = re.search('if\((.+)\)', code[0])
-
-                    # parse if and '('
                     parsed_code = tokenizer(parsed_code, 'if')
+
+                    # get expression
+                    code = re.search('if\s\((.+?)\)\s{', code[0])
+                    # '(' expression ')'
                     parsed_code = tokenizer(parsed_code, '(')
+                    parsed_code = expression(parsed_code, code.group(1))
+                    parsed_code = tokenizer(parsed_code, ')')
 
-                    # expression
-                    parsed_code = expression(parsed_code, expr)
+                    # '{' statements '}'
+                    parsed_code = tokenizer(parsed_code, "{")
+                    parsed_code = replace(parsed_code, "<statements>\n$$")
 
-                # while statement
+                # compile else
+                # else {
+                elif code[0].find('else') == 0:
+                    IfElseWhile.append('else')
+
+                    # replace "</ifstatement>" to "<keyword> else </keyword>"
+                    parsed_code[-15:] = "<keyword> else </keyword>\n$$"
+                    # parse '{'
+                    parsed_code = tokenizer(parsed_code, "{")
+                    # add <statements>
+                    parsed_code = replace(parsed_code, "<statements>\n$$")
+
+                # compile while statement
                 elif code[0].find('while') == 0:
-                    parsed_code = replace(parsed_code, "<whileStatement>\n$$")
-                    code = re.search('(while)', code[0])
+                    IfElseWhile.append('while')
 
-                # if do statement
+                    parsed_code = replace(parsed_code, "<whileStatement>\n$$")
+                    parsed_code = tokenizer(parsed_code, 'while')
+
+                    # get expression
+                    code = re.search('while\s\((.+?)\)\s{', code[0])
+                    
+                    # '(' expression ')'
+                    parsed_code = tokenizer(parsed_code, '(')
+                    parsed_code = expression(parsed_code, code.group(1))
+                    parsed_code = tokenizer(parsed_code, ')')
+
+                    parsed_code = tokenizer(parsed_code, '{')
+                    parsed_code = replace(parsed_code, "<statements>\n$$")
+                
+                # compile do statement
                 elif code[0].find('do') == 0:
                     parsed_code = replace(parsed_code, "<doStatement>\n$$")
+                    
                     # ['do', 'subroutineCall', 'parameters', ';']
-                    code = re.search('(do)\s(.+)\((.?|.+)\)(;)', code[0])
-
+                    code = re.search('(do)\s(.+)(;)', code[0])
+                    
                     # parse 'do'
-                    parsed_code = tokenizer(parsed_code, code.group(1))
-                    parsed_code = expression
-'''
+                    parsed_code = tokenizer(parsed_code, "do")
+
+                    # subroutineCall - (className|varName)'.'subroutineName '(' expressionList ')'
+                    if re.search('(.+)(\.)(.+)\((.?|.+)\)', code.group(1)):
+                        parsed_code = subroutineCall(parsed_code, code.group(1), 2)
+                    else:
+                        parsed_code = subroutineCall(parsed_code, code.group(1), 1)
+
+                    # parse ';'
+                    parsed_code = tokenizer(parsed_code, code.group(2))
+                    # add </doStatement>
+                    parsed_code = replace(parsed_code, "</doStatement>\n$$")
+                    
+                # compile return statement
+                else:
+                    # add <returnStatement>
+                    parsed_code = replace(parsed_code, "<returnStatement>\n$$")
+
+                    # parse 'return'
+                    parsed_code = tokenizer(parsed_code, 'return')
+
+                    # if not return;
+                    if ' ' in code[0]:
+                        code = re.search('return\s(.+?);', code[0])
+                        parsed_code = expression(code.group(1))
+
+                    # parse ';'
+                    parsed_code = tokenizer(parsed_code, ';')
+                    # add </returnStatement>
+                    parsed_code = replace(parsed_code, "</returnStatement>\n$$")
 
     # make file "fileName_res.xml" and copy parsed code to result file
     with open(jackFile[:-5]+'_res.xml', 'w') as res:
         res.write(parsed_code)
-
 
 # if argv[1] is file, translate
 if ".jack" in sys.argv[1]:
